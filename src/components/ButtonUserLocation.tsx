5e0da0653d98e5196fb66a8614ce0345d59d4435
Alvaro <alvarodr_97@hotmail.com>
2024-01-27 12:13:08 +0100

    feat: prettier, button user location

--- src/components/ButtonUserLocation.tsx
+++ src/components/ButtonUserLocation.tsx
@@ -1,7 +1,12 @@
 import useMapStore from "@/zustand/mapStore";
 import usePlacesStore from "@/zustand/placeStore";
-import { Button } from "./ui/button";
 import { MapPin } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
 
 export const ButtonUserLocation = () => {
   const { map, isMapReady } = useMapStore();
@@ -17,15 +22,21 @@ export const ButtonUserLocation = () => {
     });
   };
 
-  // TODO: Cambiar estilos
   return (
-    <Button
-      onClick={onClick}
-      variant="destructive"
-      className="fixed flex flex-row gap-x-2 top-4 right-4 rounded-full"
-    >
-      <MapPin />
-      <span className="hidden md:inline">Mi ubicación</span>
-    </Button>
+    <TooltipProvider delayDuration={200}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <div
+            onClick={onClick}
+            className="fixed bottom-8 right-4 flex h-14 w-14 cursor-pointer items-center justify-center rounded-full border border-black bg-white transition hover:scale-105"
+          >
+            <MapPin className="h-8 w-8" />
+          </div>
+        </TooltipTrigger>
+        <TooltipContent side="left" sideOffset={4}>
+          <p>Find my location</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
